7176cafe45a55cdaa44915afd280a1981ac5c3e4
Jakub Ogrodowczyk <sqanypl@gmail.com>
2023-10-11 19:19:28 +0200

    Now the photos are being saved to a folder 'images'

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import numpy as np
 import cv2 as cv
 import pyzbar.pyzbar as pyzbar
 import datetime
-import time
-
+import os
 
 current_time = f"[{datetime.datetime.now()}]"
 print(current_time)
+current_directory = os.getcwd()
+directory = "images"
+print(current_directory)
+path = os.path.join(current_directory, directory)
+try:
+    os.mkdir(path)
+except OSError as error:
+    print(f"{current_time}  {error}")
+
+
+color_of_qr_rectangle = (0, 0, 255)
+color_of_text = (255, 255, 255)
+thickness = 2
 
 cap = cv.VideoCapture(0)
 
@@ -26,10 +38,6 @@ while True:
     if barcode:
         print(f"{current_time}  Recognized a barcode!")
 
-    color_of_qr_rectangle = (0, 0, 255)
-    color_of_text = (255, 255, 255)
-    thickness = 2
-
     for QR in barcode:
         # x and y are upper left corner of the barcode while w and h are width and height of the barcode
         x, y, w, h = QR.rect
@@ -41,7 +49,7 @@ while True:
             frame = cap.read()[1]  # reading a new frame before updating the countdown
             cv.waitKey(1000)
             filename = f"QRPhoto_{i}.png"
-            cv.imwrite(filename, frame)
+            cv.imwrite(f"{path}/{filename}", frame)
             print(f"{current_time}  Image successfully saved as {filename}")
             if i == 0:
                 break
@@ -53,7 +61,7 @@ while True:
     cv.imshow('frame', frame)
 
     if cv.waitKey(1) == ord('q'):
-        cv.imwrite("Last_frame.png", frame)
+        cv.imwrite(f"{path}/Last_frame.png", frame)
         break
 
 # When everything done, release the capture
